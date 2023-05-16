6ebf973d6fab75f15caf15491745946d04d34dcc
LiZF <1024005498@qq.com>
2023-05-16 10:06:27 +0800

    提交信息

--- src/pages/DataAssets/Directory/index.tsx
+++ src/pages/DataAssets/Directory/index.tsx
@@ -1,32 +1,29 @@
 import { PageContainer, ProCard } from '@ant-design/pro-components';
 
-import { Button, Card, Col, Dropdown, Form, Input, message, Row, Select, Space } from 'antd';
+import { Button, Col, Form, Row, Select, Space } from 'antd';
+import '../Directory/services/index.css';
 import ProList from './components/ProList';
 import TreeData from './components/TreeData';
-import '../Directory/services/index.css';
-const { Option } = Select
+const { Option } = Select;
 export default () => {
   const onGenderChange = () => {
     console.log(123);
-  }
+  };
   return (
-    <PageContainer title="资产目录"
+    <PageContainer
+      title="资产目录"
       extra={
         <Form>
           <Form.Item name="version" label="切换版本" rules={[{ required: true }]}>
-            <Select
-              onChange={onGenderChange}
-              allowClear
-              defaultValue={2023}
-              style={{ width: 100 }}
-            >
+            <Select onChange={onGenderChange} allowClear defaultValue={2023} style={{ width: 100 }}>
               <Option value="2023">2023</Option>
               <Option value="2022">2022</Option>
               <Option value="2021">2021</Option>
             </Select>
           </Form.Item>
         </Form>
-      }>
+      }
+    >
       {/* <Card bordered>
         <Form onFinish={() => message.info('暂不支持查询')}>
           <Row>
@@ -43,7 +40,7 @@ export default () => {
           </Row>
         </Form>
       </Card> */}
-      <div style={{ padding: 0, background: '#ffffff', width: '100%',borderRadius:10}}>
+      <div style={{ padding: 0, background: '#ffffff', width: '100%', borderRadius: 10 }}>
         <Row>
           <Col span={5}>
             <ProCard>
@@ -59,7 +56,7 @@ export default () => {
                   <Select
                     onChange={onGenderChange}
                     allowClear
-                    defaultValue={"资源"}
+                    defaultValue={'资源'}
                     style={{ width: 110 }}
                   >
                     <Option value="资源">资源</Option>
@@ -72,6 +69,15 @@ export default () => {
             </ProCard>
           </Col>
           <Col span={19}>
+            <br></br>
+            <Row>
+              <Col offset={21}>
+                <Space>
+                  <Button>导入</Button>
+                  <Button>导出</Button>
+                </Space>
+              </Col>
+            </Row>
             <ProList />
           </Col>
         </Row>
