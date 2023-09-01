315a9d45891b3d531d0cf13ec162f24233b86abe
jinjjm <1185838762@qq.com>
2023-09-01 16:13:51 +0800

    aaaa

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,10 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import { useIntl } from '@umijs/max';
+import { Image } from 'antd';
 import React from 'react';
+
+import logo from './gm-logo.png'
 
 const Footer: React.FC = () => {
   const intl = useIntl();
@@ -18,24 +21,25 @@ const Footer: React.FC = () => {
       style={{
         background: 'none',
       }}
-      copyright={`${currentYear} ${defaultMessage}`}
+      copyright={`${currentYear} ${defaultMessage}`} // 版权
+      // copyright={false}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'tjgl',
+          title: '天津公路事业发展服务中心',
+          href: 'https://baike.baidu.com/item/%E5%A4%A9%E6%B4%A5%E5%B8%82%E5%85%AC%E8%B7%AF%E4%BA%8B%E4%B8%9A%E5%8F%91%E5%B1%95%E6%9C%8D%E5%8A%A1%E4%B8%AD%E5%BF%83',
           blankTarget: true,
         },
         {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          key: 'logo',
+          title: <Image src={logo} preview={false} width={20}/>,
+          href: 'https://jtys.tj.gov.cn/',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'hebut',
+          title: '河北工业大学',
+          href: 'https://www.hebut.edu.cn/',
           blankTarget: true,
         },
       ]}
